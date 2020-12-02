44458ec843e8293acae6deae476c875f181c6d2a
Alex_Karavaev <dendi.alexei@yandex.ru>
2020-12-02 18:36:16 +0300

    4

---
+++ Задание 1.py
@@ -0,0 +1,11 @@
+#Задача1
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+def my_min(a,b,c,d):
+  x=min(a,b)
+  y=min(c,d)
+  z=min(x,y)
+  return(z)
+print(my_min(a,b,c,d))
---
+++ Задание 2.py
@@ -0,0 +1,12 @@
+#Задача2
+a=int(input())
+def my_div(a):
+  div='no div'
+  for i in range(2,a-1):
+    if a%1==0:
+      div=1
+  if div=='no div':
+    return(a)
+  else:
+    return(div)
+print(my_div(a))
